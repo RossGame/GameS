bb1b80bcff54d923366e6525fd63b563d3bc8b66
rosssong1984 <7993402@qq.com>
2015-11-25 16:26:43 +0800

    1

---
+++ Assets/Scripts/MapControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapControl : MonoBehaviour {
+
+    private TextAsset map_1;
+
+	// Use this for initialization
+	void Start () {
+
+        GetMap("TestMap");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private TextAsset GetMap(string mapName)
+    {
+        TextAsset map = Resources.Load("Map/" + mapName, typeof(TextAsset)) as TextAsset;
+
+        return map;
+    }
+}
