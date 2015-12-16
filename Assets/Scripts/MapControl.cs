bd8f0d29dcb4d42d4f019008de93aa518ff927e5
rosssong1984 <7993402@qq.com>
2015-12-16 14:53:59 +0800

    ReadMap

--- Assets/Scripts/MapControl.cs
+++ Assets/Scripts/MapControl.cs
@@ -1,14 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using Assets.Scripts.Objects;
 public class MapControl : MonoBehaviour {
+
+    public GameObject floor;
+    public GameObject box;
 
     private TextAsset map_1;
 
 	// Use this for initialization
 	void Start () {
 
-        GetMap("TestMap");
+        Map map = new Map(GetMap("TestMap"));
+        CreateMap(map);
     }
 	
 	// Update is called once per frame
@@ -19,7 +23,40 @@ public class MapControl : MonoBehaviour {
     private TextAsset GetMap(string mapName)
     {
         TextAsset map = Resources.Load("Map/" + mapName, typeof(TextAsset)) as TextAsset;
-
+       
         return map;
+    }
+
+    private void CreateMap(Map map)
+    {
+        for(int x = 0; x<map.MapMaxX; x++)
+        {
+            for(int y = 0; y<map.MapMaxY; y++)
+            {
+                string mapType = map.MapContent[x, y];
+
+                switch(mapType)
+                {
+                    case "1":
+                        CreatFloor(new Vector3(y, 0 , x));
+                        break;
+                    case "*":
+                        CreatBox(new Vector3(y, 0, x));
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    private void CreatFloor(Vector3 coordinate)
+    {
+        Instantiate(floor, coordinate, new Quaternion());
+    }
+
+    private void CreatBox(Vector3 coordinate)
+    {
+        Instantiate(box, coordinate, new Quaternion());
     }
 }
---
+++ Assets/Scripts/Objects/Map.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Objects
+{
+    public class Map
+    {
+        private string[,] mapContent;
+
+        public int MapMaxX
+        {
+            get { return Convert.ToInt32(mapContent.GetLongLength(0)); }
+            //set { mapMaxX = value; }
+        }
+
+        public int MapMaxY
+        {
+            get { return Convert.ToInt32(mapContent.GetLongLength(1)); }
+            //set { mapMaxY = value; }
+        }
+
+        public string[,] MapContent
+        {
+            get { return mapContent; }
+        }
+
+        public Map(TextAsset mapAsset)
+        {
+            string [] rows = mapAsset.text.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            int mapMaxY = rows.Length;
+            int mapMaxX = rows[0].Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries).Length;
+
+            mapContent = new string[mapMaxX, mapMaxY];
+
+            for(int y = 0; y< mapContent.GetLength(1); y++)
+            {
+                string[] cols = rows[y].Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+
+                for (int x = 0; x< mapContent.GetLongLength(0); x++)
+                {
+                    mapContent[x, y] = cols[x] as string;
+                }
+            }
+        }
+    
+    }
+}
